d598adb6a86e3f8f6dc36c92a93cfc4e909dd863
karthiknayakniveus <karthik.nayak@niveussolutions.com>
2023-09-04 15:03:06 +0530

    updated download_file_list.ps1 file

--- download_file_list.ps1
+++ download_file_list.ps1
@@ -2,13 +2,12 @@
 $commit1 = (git log --pretty=format:%H -n 1 HEAD~0)
 $commit2 = (git log --pretty=format:%H -n 1 HEAD~1)
 
-# Create the directory to store downloaded files
-$downloadDirectory = "C:\path\to\your\directory"  # Replace with your desired directory path
-New-Item -ItemType Directory -Force -Path $downloadDirectory
+$downloadDirectory = "/home/karthik/Downloadfilelist"
 
-# Loop through the added files and copy them to the directory
-git diff --name-only $commit1 $commit2 | ForEach-Object {
-    $file = $_
+$addedFiles = git diff --name-only $commit1 $commit2
+
+foreach ($file in $addedFiles) {
+    $sourcePath = "git show $($commit2):$file"
     $targetPath = Join-Path -Path $downloadDirectory -ChildPath $file
-    git show "$commit2:$file" | Set-Content -Path $targetPath
+    Invoke-Expression -Command $sourcePath | Out-File -FilePath $targetPath
 }
